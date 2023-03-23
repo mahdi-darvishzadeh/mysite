b6ebb8820fb2de3936189d89038e7aabc0b402fa
mahdi <darvishzadeh021@gmail.com>
2023-03-24 00:58:53 +0330

    created render html files
    added templates folder and html files to website app and render to views

--- website/views.py
+++ website/views.py
@@ -4,11 +4,11 @@ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
 def index_view(request):
-    return HttpResponse('<h1>Home page</h1>')
+    return render(request, 'website/index.html')
 
 
 def about_view(request):
-    return HttpResponse('<h1>About page</h1>')
+    return render(request, 'website/about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1>Contact page</h1>')
+    return render(request, 'website/contact.html')
