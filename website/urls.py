89cf31b02ce99f5ab20980374e3e69d79ce6c709
mahdi <darvishzadeh021@gmail.com>
2023-03-25 22:52:28 +0330

    added example templates by project

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', index_view),
     path('about', about_view),
     path('contact', contact_view),
+    path('elements', elements_view),
 ]
--- website/views.py
+++ website/views.py
@@ -6,9 +6,11 @@ from django.http import HttpResponse, JsonResponse
 def index_view(request):
     return render(request, 'website/index.html')
 
-
 def about_view(request):
     return render(request, 'website/about.html')
 
 def contact_view(request):
     return render(request, 'website/contact.html')
+
+def elements_view(request):
+    return render(request, 'website/elements.html')
